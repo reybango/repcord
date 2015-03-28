ef266a8af5339d5c41199d64987803f5719a3eaa
Giancarlo Gomez <giancarlo.gomez@gmail.com>
2015-03-28 05:08:42 -0400

    Went ahead and enabled the parallax type of effect on small views by adding a listener on view resize and applying required height and position updates to background holding divs

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,31 @@ $(function() {
 	// Get the messages div.
 	var formMessages = $('#form-messages');
 
+	// backgrounds
+	var backgrounds = {
+			b1 : $('#background-1'),
+			b2 : $('#background-2'),
+			b3 : $('#background-3')
+		},
+		contents = {
+			c1 : $('#content-1'),
+			c2 : $('#content-3'),
+			c3 : $('#content-5')
+		};
+
+	on_resize(function() {
+		console.log('i fired');
+		if(window.innerWidth < 850){
+			backgrounds.b1.height(contents.c1.outerHeight()).css('background-size','auto 100%');
+			backgrounds.b2.height(contents.c2.outerHeight()).css('top',contents.c2.position().top);
+			backgrounds.b3.height(contents.c3.outerHeight()).css('top',contents.c3.position().top);
+		} else {
+			backgrounds.b1.attr('style','');
+			backgrounds.b2.attr('style','');
+			backgrounds.b3.attr('style','');
+		}
+	})();
+
 	// Set up an event listener for the contact form.
 	$(form).submit(function(e) {
 		// Stop the browser from submitting the form.
@@ -49,3 +74,6 @@ $(function() {
 	});
 
 });
+
+// debulked onresize handler
+function on_resize(c,t){onresize=function(){clearTimeout(t);t=setTimeout(c,100)};return c};
